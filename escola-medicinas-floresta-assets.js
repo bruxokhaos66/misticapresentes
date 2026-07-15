e5460ec7858a0e85a29372c3b5d89695f8750e86
bruxokhaos66 <fredineibach@gmail.com>
2026-07-15 17:32:20 -0300

    feat(escola): prepara mapeamento de imagens da aula de rapé e ayahuasca

---
+++ escola-medicinas-floresta-assets.js
@@ -0,0 +1,10 @@
+(() => {
+  "use strict";
+
+  // As fotografias oficiais serão adicionadas pelo Cloud em etapa posterior.
+  // Enquanto isso, os valores vazios mantêm os gradientes premium sem gerar 404.
+  window.MEDICINAS_FLORESTA_ASSETS = Object.freeze({
+    curso: "",
+    aula: "",
+  });
+})();
